cda201dc82d3d95fa570809d46ddb7210b5b3416
Gordon Smith <gordonsmith@apache.org>
2012-11-02 21:15:00 +0000

    Falcon: Added five nontrivial tests for parsing <Vector> tag.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flex/falcon/trunk@1405169 13f79535-47bb-0310-9956-ffa450edef68

--- compiler.tests/unit-tests/org/apache/flex/compiler/internal/tree/mxml/MXMLVectorNodeTests.java
+++ compiler.tests/unit-tests/org/apache/flex/compiler/internal/tree/mxml/MXMLVectorNodeTests.java
@@ -22,7 +22,11 @@ package org.apache.flex.compiler.internal.tree.mxml;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import org.apache.flex.compiler.definitions.ITypeDefinition;
 import org.apache.flex.compiler.tree.ASTNodeID;
+import org.apache.flex.compiler.tree.mxml.IMXMLBooleanNode;
+import org.apache.flex.compiler.tree.mxml.IMXMLIntNode;
+import org.apache.flex.compiler.tree.mxml.IMXMLStringNode;
 import org.apache.flex.compiler.tree.mxml.IMXMLVectorNode;
 import org.apache.flex.compiler.tree.mxml.IMXMLFileNode;
 import org.junit.Test;
@@ -34,6 +38,8 @@ import org.junit.Test;
  */
 public class MXMLVectorNodeTests extends MXMLInstanceNodeTests
 {
+	private static String EOL = "\n\t\t";
+	
 	private IMXMLVectorNode getMXMLVectorNode(String code)
 	{
 		IMXMLFileNode fileNode = getMXMLFileNode(code);
@@ -65,5 +71,101 @@ public class MXMLVectorNodeTests extends MXMLInstanceNodeTests
 		String code = "<fx:Vector> \t\r\n</fx:Vector>";
 		IMXMLVectorNode node = getMXMLVectorNode(code);
 		assertThat("getChildCount", node.getChildCount(), is(0));
+	}
+	
+	@Test
+	public void MXMLVectorNode_no_type()
+	{
+		String code =
+			"<fx:Vector>" + EOL +
+		    "    <fx:Boolean>true</fx:Boolean>" + EOL +
+			"    <fx:int>123</fx:int>" + EOL +
+		    "    <fx:String>abc</fx:String>" + EOL +
+			"</fx:Vector>";
+		IMXMLVectorNode node = getMXMLVectorNode(code);
+		assertThat("getType", node.getType(), is((ITypeDefinition)null));
+		assertThat("getFixed", node.getFixed(), is(false));
+		assertThat("getChildCount", node.getChildCount(), is(3));
+		assertThat("[0]", ((IMXMLBooleanNode)node.getChild(0)).getValue(), is(true));
+		assertThat("[1]", ((IMXMLIntNode)node.getChild(1)).getValue(), is(123));
+		assertThat("[2]", ((IMXMLStringNode)node.getChild(2)).getValue(), is("abc"));
+	}
+	
+	@Test
+	public void MXMLVectorNode_Object_type()
+	{
+		String code =
+			"<fx:Vector type='Object'>" + EOL +
+		    "    <fx:Boolean>true</fx:Boolean>" + EOL +
+			"    <fx:int>123</fx:int>" + EOL +
+		    "    <fx:String>abc</fx:String>" + EOL +
+			"</fx:Vector>";
+		IMXMLVectorNode node = getMXMLVectorNode(code);
+		assertThat("getType", node.getType().getQualifiedName(), is("Object"));
+		assertThat("getFixed", node.getFixed(), is(false));
+		assertThat("getChildCount", node.getChildCount(), is(3));
+		assertThat("[0]", ((IMXMLBooleanNode)node.getChild(0)).getValue(), is(true));
+		assertThat("[1]", ((IMXMLIntNode)node.getChild(1)).getValue(), is(123));
+		assertThat("[2]", ((IMXMLStringNode)node.getChild(2)).getValue(), is("abc"));
+	}
+	
+	@Test
+	public void MXMLVectorNode_star_type()
+	{
+		String code =
+			"<fx:Vector type='*'>" + EOL +
+		    "    <fx:Boolean>true</fx:Boolean>" + EOL +
+			"    <fx:int>123</fx:int>" + EOL +
+		    "    <fx:String>abc</fx:String>" + EOL +
+			"</fx:Vector>";
+		IMXMLVectorNode node = getMXMLVectorNode(code);
+		assertThat("getType", node.getType().getQualifiedName(), is("*"));
+		assertThat("getFixed", node.getFixed(), is(false));
+		assertThat("getChildCount", node.getChildCount(), is(3));
+		assertThat("[0]", ((IMXMLBooleanNode)node.getChild(0)).getValue(), is(true));
+		assertThat("[1]", ((IMXMLIntNode)node.getChild(1)).getValue(), is(123));
+		assertThat("[2]", ((IMXMLStringNode)node.getChild(2)).getValue(), is("abc"));
+	}
+	
+	@Test
+	public void MXMLVectorNode_int_type()
+	{
+		String code =
+			"<fx:Vector type='int'>" + EOL +
+		    "    <fx:int>1</fx:int>" + EOL +
+			"    <fx:int>2</fx:int>" + EOL +
+		    "    <fx:int>3</fx:int>" + EOL +
+			"</fx:Vector>";
+		IMXMLVectorNode node = getMXMLVectorNode(code);
+		assertThat("getType", node.getType().getQualifiedName(), is("int"));
+		assertThat("getFixed", node.getFixed(), is(false));
+		assertThat("getChildCount", node.getChildCount(), is(3));
+		assertThat("[0]", ((IMXMLIntNode)node.getChild(0)).getValue(), is(1));
+		assertThat("[1]", ((IMXMLIntNode)node.getChild(1)).getValue(), is(2));
+		assertThat("[2]", ((IMXMLIntNode)node.getChild(2)).getValue(), is(3));
+	}
+	
+	@Test
+	public void MXMLVectorNode_nested_int_vectors()
+	{
+		String code =
+			"<fx:Vector type='int'>" + EOL +
+		    "    <fx:Vector type='int'>" + EOL +
+		    "        <fx:int>1</fx:int>" + EOL +
+		    "        <fx:int>2</fx:int>" + EOL +
+		    "    </fx:Vector>" + EOL +
+		    "    <fx:Vector type='int'>" + EOL +
+		    "        <fx:int>3</fx:int>" + EOL +
+		    "        <fx:int>4</fx:int>" + EOL +
+		    "    </fx:Vector>" + EOL +
+			"</fx:Vector>";
+		IMXMLVectorNode node = getMXMLVectorNode(code);
+		assertThat("getChildCount", node.getChildCount(), is(2));
+		assertThat("[0].getChildCount", ((IMXMLVectorNode)node.getChild(0)).getChildCount(), is(2));
+		assertThat("[0][0]", ((IMXMLIntNode)node.getChild(0).getChild(0)).getValue(), is(1));
+		assertThat("[0][1]", ((IMXMLIntNode)node.getChild(0).getChild(1)).getValue(), is(2));
+		assertThat("[1].getChildCount", ((IMXMLVectorNode)node.getChild(1)).getChildCount(), is(2));
+		assertThat("[1][0]", ((IMXMLIntNode)node.getChild(1).getChild(0)).getValue(), is(3));
+		assertThat("[1][1]", ((IMXMLIntNode)node.getChild(1).getChild(1)).getValue(), is(4));
 	}
 }
