140c69c3d8f667471309b4bfbe9b877099ad2876
Gordon Smith <gordonsmith@apache.org>
2012-11-02 21:05:13 +0000

    Falcon: Added two nontrivial tests for parsing <Array> tag.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flex/falcon/trunk@1405166 13f79535-47bb-0310-9956-ffa450edef68

--- compiler.tests/unit-tests/org/apache/flex/compiler/internal/tree/mxml/MXMLArrayNodeTests.java
+++ compiler.tests/unit-tests/org/apache/flex/compiler/internal/tree/mxml/MXMLArrayNodeTests.java
@@ -24,7 +24,10 @@ import static org.junit.Assert.assertThat;
 
 import org.apache.flex.compiler.tree.ASTNodeID;
 import org.apache.flex.compiler.tree.mxml.IMXMLArrayNode;
+import org.apache.flex.compiler.tree.mxml.IMXMLBooleanNode;
 import org.apache.flex.compiler.tree.mxml.IMXMLFileNode;
+import org.apache.flex.compiler.tree.mxml.IMXMLIntNode;
+import org.apache.flex.compiler.tree.mxml.IMXMLStringNode;
 import org.junit.Test;
 
 /**
@@ -34,6 +37,8 @@ import org.junit.Test;
  */
 public class MXMLArrayNodeTests extends MXMLInstanceNodeTests
 {
+	private static String EOL = "\n\t\t";
+	
 	private IMXMLArrayNode getMXMLArrayNode(String code)
 	{
 		IMXMLFileNode fileNode = getMXMLFileNode(code);
@@ -65,5 +70,45 @@ public class MXMLArrayNodeTests extends MXMLInstanceNodeTests
 		String code = "<fx:Array> \t\r\n</fx:Array>";
 		IMXMLArrayNode node = getMXMLArrayNode(code);
 		assertThat("getChildCount", node.getChildCount(), is(0));
+	}
+	
+	@Test
+	public void MXMLArrayNode_three_inhomogenous_elements()
+	{
+		String code =
+			"<fx:Array>" + EOL +
+		    "    <fx:Boolean>true</fx:Boolean>" + EOL +
+			"    <fx:int>123</fx:int>" + EOL +
+		    "    <fx:String>abc</fx:String>" + EOL +
+			"</fx:Array>";
+		IMXMLArrayNode node = getMXMLArrayNode(code);
+		assertThat("getChildCount", node.getChildCount(), is(3));
+		assertThat("[0]", ((IMXMLBooleanNode)node.getChild(0)).getValue(), is(true));
+		assertThat("[1]", ((IMXMLIntNode)node.getChild(1)).getValue(), is(123));
+		assertThat("[2]", ((IMXMLStringNode)node.getChild(2)).getValue(), is("abc"));
+	}
+	
+	@Test
+	public void MXMLArrayNode_nested_arrays()
+	{
+		String code =
+			"<fx:Array>" + EOL +
+		    "    <fx:Array>" + EOL +
+		    "        <fx:int>1</fx:int>" + EOL +
+		    "        <fx:int>2</fx:int>" + EOL +
+		    "    </fx:Array>" + EOL +
+		    "    <fx:Array>" + EOL +
+		    "        <fx:int>3</fx:int>" + EOL +
+		    "        <fx:int>4</fx:int>" + EOL +
+		    "    </fx:Array>" + EOL +
+			"</fx:Array>";
+		IMXMLArrayNode node = getMXMLArrayNode(code);
+		assertThat("getChildCount", node.getChildCount(), is(2));
+		assertThat("[0].getChildCount", ((IMXMLArrayNode)node.getChild(0)).getChildCount(), is(2));
+		assertThat("[0][0]", ((IMXMLIntNode)node.getChild(0).getChild(0)).getValue(), is(1));
+		assertThat("[0][1]", ((IMXMLIntNode)node.getChild(0).getChild(1)).getValue(), is(2));
+		assertThat("[1].getChildCount", ((IMXMLArrayNode)node.getChild(1)).getChildCount(), is(2));
+		assertThat("[1][0]", ((IMXMLIntNode)node.getChild(1).getChild(0)).getValue(), is(3));
+		assertThat("[1][1]", ((IMXMLIntNode)node.getChild(1).getChild(1)).getValue(), is(4));
 	}
 }
