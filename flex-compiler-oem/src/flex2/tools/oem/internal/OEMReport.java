fdf521a7c9dac8f908fbecba4e444d1a55301f70
Alex Harui <aharui@apache.org>
2014-04-07 21:40:12 -0700

    remove unused import

--- flex-compiler-oem/src/flex2/tools/oem/internal/OEMReport.java
+++ flex-compiler-oem/src/flex2/tools/oem/internal/OEMReport.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
