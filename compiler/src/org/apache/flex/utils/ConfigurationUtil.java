a1dae4719cf6bca45a09fc90e9c4b1aff6b3acd8
Frédéric THOMAS <webdoublefx@gmail.com>
2015-05-30 16:52:52 +0100

    Cleanup: - Added Apache headers - Removed @Author tag

--- compiler/src/org/apache/flex/utils/ConfigurationUtil.java
+++ compiler/src/org/apache/flex/utils/ConfigurationUtil.java
@@ -1,24 +1,35 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.apache.flex.utils;
 
 import java.io.File;
 
-/**
- * @author: Frederic Thomas
- * Date: 29/05/2015
- * Time: 17:04
- */
 public class ConfigurationUtil {
 
     // workaround for Falcon bug.
     // Input files with relative paths confuse the algorithm that extracts the root class name.
-    public static String[] fixArgs(final String[] args)
-    {
+    public static String[] fixArgs(final String[] args) {
         String[] newArgs = args;
-        if (args.length > 1)
-        {
+        if (args.length > 1) {
             String targetPath = args[args.length - 1];
-            if (targetPath.startsWith("."))
-            {
+            if (targetPath.startsWith(".")) {
                 targetPath = FileUtils.getTheRealPathBecauseCanonicalizeDoesNotFixCase(new File(targetPath));
                 newArgs = new String[args.length];
                 for (int i = 0; i < args.length - 1; ++i)
--- flex-compiler-oem/src/flex2/tools/MxmlJSC.java
+++ flex-compiler-oem/src/flex2/tools/MxmlJSC.java
@@ -1,9 +1,28 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package flex2.tools;
 
-import org.apache.flex.compiler.clients.problems.ProblemQueryProvider;
 import org.apache.flex.compiler.clients.JSCompilerEntryPoint;
 import org.apache.flex.compiler.clients.MXMLJSC;
 import org.apache.flex.compiler.clients.problems.ProblemQuery;
+import org.apache.flex.compiler.clients.problems.ProblemQueryProvider;
 import org.apache.flex.compiler.driver.IBackend;
 import org.apache.flex.compiler.internal.driver.as.ASBackend;
 import org.apache.flex.compiler.internal.driver.js.amd.AMDBackend;
@@ -16,11 +35,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * @author: Frederic Thomas
- * Date: 25/05/2015
- * Time: 14:05
- */
 public class MxmlJSC implements ProblemQueryProvider {
 
     protected static Class<? extends MXMLJSC> COMPILER = MXMLJSC.class;
