604f09232da05988eb66f2a2c35fc352eb5da0d4
Alex Harui <aharui@apache.org>
2015-06-01 10:37:17 -0700

    re-apply 06aa01a8dd3104c11b03ea08258ee9bae40c287d from Flex SDK

--- debugger/src/flex/tools/debugger/cli/DebugCLI.java
+++ debugger/src/flex/tools/debugger/cli/DebugCLI.java
@@ -1335,7 +1335,7 @@ public class DebugCLI implements Runnable, SourceLocator
 			{
 				// keep spitting out frames until we can't
 				Frame frame = stack[i];
-				boolean valid = appendFrameInfo(sb, frame, i, showThis, false);
+				boolean valid = appendFrameInfo(sb, frame, i, showThis, true);
 				sb.append(m_newline);
                 if (!valid)
                     break;
