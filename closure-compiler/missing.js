513a9b343805e4fa9393656d7d59f4a3e3b8263f
Alex Harui <aharui@apache.org>
2015-06-19 23:14:34 -0700

    check files into closure-compiler so it can be built from command-line

---
+++ closure-compiler/missing.js
@@ -0,0 +1,97 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+ 
+// webkit_notifications
+/**
+ * @constructor
+ */
+function ExtendableEvent() {}
+
+// HACK @enum page_visibility
+/**
+ * @constructor
+ */
+function VisibilityState(description) {}
+
+
+// chrome.js
+/**
+ * @constructor
+ */
+function HTMLEmbedElement() {};
+
+/**
+ * @type {!Window}
+ * @const
+ */
+var window;
+
+/**
+ * @type {!HTMLDocument}
+ */
+Window.prototype.document;
+
+/**
+ * @constructor
+ */
+function Promise() {}
+
+/**
+ * @constructor
+ */
+function FontFaceSetLoadStatus() {}
+
+/**
+ * @constructor
+ */
+function FontFaceLoadStatus() {}
+
+/**
+ * @constructor
+ */
+function Navigator() {}
+
+
+/**
+ * @constructor
+ */
+function Screen() {}
+
+/**
+ * @constructor
+ */
+function uint() {}
+
+/**
+ * @constructor
+ */
+function int() {}
+
+// gecko
+
+/**
+ * @constructor
+ */
+function History() {}
+
+/**
+ * @constructor
+ */
+function Location() {}
