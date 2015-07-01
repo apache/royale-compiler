7ad42479d865f621cb4ef61b859bf4168e74ae25
Frédéric THOMAS <webdoublefx@gmail.com>
2015-07-01 06:06:00 +0100

    ADDING A CONSOLE LOGGER
    Add a logger if none have been added,
    in this case we are probably in console
     instead of IntelliJ.

--- flex-compiler-oem/src/flex2/tools/Tool.java
+++ flex-compiler-oem/src/flex2/tools/Tool.java
@@ -19,13 +19,15 @@
 
 package flex2.tools;
 
-import flash.localization.LocalizationManager;
-import flex2.compiler.Logger;
-import flex2.compiler.config.ConfigurationException;
-import flex2.compiler.util.CompilerMessage;
-import flex2.compiler.util.CompilerMessage.CompilerError;
-import flex2.compiler.util.ThreadLocalToolkit;
-import flex2.tools.oem.Message;
+import static org.apache.flex.compiler.clients.MXMLJSC.JSOutputType.FLEXJS_DUAL;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
 import org.apache.flex.compiler.clients.COMPC;
 import org.apache.flex.compiler.clients.MXMLC;
 import org.apache.flex.compiler.clients.MXMLJSC.JSOutputType;
@@ -41,23 +43,26 @@ import org.apache.flex.compiler.problems.ICompilerProblem;
 import org.apache.flex.compiler.problems.annotations.DefaultSeverity;
 import org.apache.flex.utils.ArgumentUtil;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
-
-import static org.apache.flex.compiler.clients.MXMLJSC.JSOutputType.FLEXJS_DUAL;
+import flash.localization.LocalizationManager;
+import flex2.compiler.Logger;
+import flex2.compiler.config.ConfigurationException;
+import flex2.compiler.util.CompilerMessage;
+import flex2.compiler.util.CompilerMessage.CompilerError;
+import flex2.compiler.util.ConsoleLogger;
+import flex2.compiler.util.ThreadLocalToolkit;
+import flex2.tools.oem.Message;
 
 /**
  * Common base class for most flex tools.
  */
-public class Tool {
+public class Tool
+{
     protected static Class<? extends MXMLC> COMPILER;
     protected static Class<? extends MxmlJSC> JS_COMPILER;
 
-    protected static int compile(String[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    protected static int compile(String[] args) throws NoSuchMethodException, InstantiationException,
+            IllegalAccessException, InvocationTargetException
+    {
         int exitCode;
 
         final String value = ArgumentUtil.getValue(args, "-js-output-type");
@@ -68,48 +73,64 @@ public class Tool {
             jsOutputType = JSOutputType.fromString(value);
         }
 
-        if (jsOutputType != null) {
+        if (jsOutputType != null)
+        {
             ArgumentBag bag = preparePhase1(new ArgumentBag(args));
 
             MxmlJSC mxmlJSC = JS_COMPILER.newInstance();
             exitCode = mxmlJSC.execute(bag.args);
 
-            if (!mxmlJSC.getProblemQuery().hasErrors()) {
-                if (jsOutputType.equals(FLEXJS_DUAL)) {
+            if (!mxmlJSC.getProblemQuery().hasErrors())
+            {
+                if (jsOutputType.equals(FLEXJS_DUAL))
+                {
                     preparePhase2(bag);
                     exitCode = flexCompile(bag.args);
                 }
-            } else {
+            }
+            else
+            {
                 processProblemReport(mxmlJSC);
             }
-        } else {
+        }
+        else
+        {
             exitCode = flexCompile(args);
         }
 
         return exitCode;
     }
 
-    protected static ArgumentBag preparePhase1(ArgumentBag bag) {
+    protected static ArgumentBag preparePhase1(ArgumentBag bag)
+    {
         bag.isCommandLineOutput = true;
 
         bag.oldOutputPath = ArgumentUtil.getValue(bag.args, "-output");
 
-        if (bag.oldOutputPath == null) {
+        if (bag.oldOutputPath == null)
+        {
             bag.oldOutputPath = ArgumentUtil.getValue(bag.args, "-o");
-            if (bag.oldOutputPath != null) {
+            if (bag.oldOutputPath != null)
+            {
                 bag.outputAlias = "-o";
             }
-        } else {
+        }
+        else
+        {
             bag.outputAlias = "-output";
         }
 
-        if (bag.oldOutputPath == null) {
+        if (bag.oldOutputPath == null)
+        {
             bag.isCommandLineOutput = false;
             ConfigurationBuffer flexConfig = null;
 
-            try {
+            try
+            {
                 flexConfig = loadConfig(bag.args);
-            } catch (org.apache.flex.compiler.exceptions.ConfigurationException ignored) {
+            }
+            catch (org.apache.flex.compiler.exceptions.ConfigurationException ignored)
+            {
             }
 
             final List<ConfigurationValue> output = flexConfig != null ? flexConfig.getVar("output") : null;
@@ -117,15 +138,21 @@ public class Tool {
             bag.oldOutputPath = configurationValue != null ? configurationValue.getArgs().get(0) : null;
         }
 
-        if (bag.oldOutputPath != null) {
+        if (bag.oldOutputPath != null)
+        {
             final int lastIndexOf = Math.max(bag.oldOutputPath.lastIndexOf("/"), bag.oldOutputPath.lastIndexOf("\\"));
 
-            if (lastIndexOf > -1) {
-                bag.newOutputPath = bag.oldOutputPath.substring(0, lastIndexOf) + File.separator + "js" + File.separator + "out";
+            if (lastIndexOf > -1)
+            {
+                bag.newOutputPath = bag.oldOutputPath.substring(0, lastIndexOf) + File.separator + "js"
+                        + File.separator + "out";
 
-                if (bag.isCommandLineOutput) {
+                if (bag.isCommandLineOutput)
+                {
                     ArgumentUtil.setValue(bag.args, bag.outputAlias, bag.newOutputPath);
-                } else {
+                }
+                else
+                {
                     bag.args = ArgumentUtil.addValueAt(bag.args, "-output", bag.newOutputPath, bag.args.length - 1);
                 }
             }
@@ -134,47 +161,53 @@ public class Tool {
         return bag;
     }
 
-    protected static ArgumentBag preparePhase2(ArgumentBag bag) {
+    protected static ArgumentBag preparePhase2(ArgumentBag bag)
+    {
         bag.args = ArgumentUtil.removeElement(bag.args, "-js-output-type");
 
-        if (bag.oldOutputPath != null) {
-            if (bag.isCommandLineOutput) {
+        if (bag.oldOutputPath != null)
+        {
+            if (bag.isCommandLineOutput)
+            {
                 ArgumentUtil.setValue(bag.args, bag.outputAlias, bag.oldOutputPath);
-            } else {
+            }
+            else
+            {
                 bag.args = ArgumentUtil.removeElement(bag.args, "-output");
             }
         }
 
-        if (COMPILER.getName().equals(COMPC.class.getName())) {
-            bag.args = ArgumentUtil.addValueAt(bag.args, "-include-file", "js" + File.separator + "out" + File.separator + "*," + bag.newOutputPath, bag.args.length - 1);
+        if (COMPILER.getName().equals(COMPC.class.getName()))
+        {
+            bag.args = ArgumentUtil.addValueAt(bag.args, "-include-file", "js" + File.separator + "out"
+                    + File.separator + "*," + bag.newOutputPath, bag.args.length - 1);
         }
 
         return bag;
     }
 
-    private static ConfigurationBuffer loadConfig(String[] args) throws org.apache.flex.compiler.exceptions.ConfigurationException {
+    private static ConfigurationBuffer loadConfig(String[] args)
+            throws org.apache.flex.compiler.exceptions.ConfigurationException
+    {
         final String configFilePath = ArgumentUtil.getValue(args, "-load-config");
         final File configFile = new File(configFilePath);
         final FileSpecification fileSpecification = new FileSpecification(configFile.getAbsolutePath());
         final ConfigurationBuffer cfgbuf = createConfigurationBuffer(Configuration.class);
 
-        FileConfigurator.load(
-                cfgbuf,
-                fileSpecification,
-                new File(configFile.getPath()).getParent(),
-                "flex-config",
+        FileConfigurator.load(cfgbuf, fileSpecification, new File(configFile.getPath()).getParent(), "flex-config",
                 false);
 
         return cfgbuf;
     }
 
-    private static ConfigurationBuffer createConfigurationBuffer(
-            Class<? extends Configuration> configurationClass) {
-        return new ConfigurationBuffer(
-                configurationClass, Configuration.getAliases());
+    private static ConfigurationBuffer createConfigurationBuffer(Class<? extends Configuration> configurationClass)
+    {
+        return new ConfigurationBuffer(configurationClass, Configuration.getAliases());
     }
 
-    protected static int flexCompile(String[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    protected static int flexCompile(String[] args) throws NoSuchMethodException, InstantiationException,
+            IllegalAccessException, InvocationTargetException
+    {
         int exitCode;
 
         MXMLC mxmlc = COMPILER.newInstance();
@@ -184,32 +217,44 @@ public class Tool {
         return exitCode;
     }
 
-    public static Map<String, String> getLicenseMapFromFile(String fileName) throws ConfigurationException {
+    public static Map<String, String> getLicenseMapFromFile(String fileName) throws ConfigurationException
+    {
         Map<String, String> result = null;
         FileInputStream in = null;
 
-        try {
+        try
+        {
             in = new FileInputStream(fileName);
             Properties properties = new Properties();
             properties.load(in);
             Enumeration enumeration = properties.propertyNames();
 
-            if (enumeration.hasMoreElements()) {
+            if (enumeration.hasMoreElements())
+            {
                 result = new HashMap<String, String>();
 
-                while (enumeration.hasMoreElements()) {
+                while (enumeration.hasMoreElements())
+                {
                     String propertyName = (String) enumeration.nextElement();
                     result.put(propertyName, properties.getProperty(propertyName));
                 }
             }
-        } catch (IOException ioException) {
+        }
+        catch (IOException ioException)
+        {
             LocalizationManager l10n = ThreadLocalToolkit.getLocalizationManager();
             throw new ConfigurationException(l10n.getLocalizedTextString(new FailedToLoadLicenseFile(fileName)));
-        } finally {
-            if (in != null) {
-                try {
+        }
+        finally
+        {
+            if (in != null)
+            {
+                try
+                {
                     in.close();
-                } catch (IOException ioe) {
+                }
+                catch (IOException ioe)
+                {
                 }
             }
         }
@@ -217,24 +262,30 @@ public class Tool {
         return result;
     }
 
-    public static class FailedToLoadLicenseFile extends CompilerError {
+    public static class FailedToLoadLicenseFile extends CompilerError
+    {
         private static final long serialVersionUID = -2980033917773108328L;
 
         public String fileName;
 
-        public FailedToLoadLicenseFile(String fileName) {
+        public FailedToLoadLicenseFile(String fileName)
+        {
             super();
             this.fileName = fileName;
         }
     }
 
-    public static void processProblemReport(ProblemQueryProvider requestableProblems) {
-        for (ICompilerProblem problem : requestableProblems.getProblemQuery().getProblems()) {
+    public static void processProblemReport(ProblemQueryProvider requestableProblems)
+    {
+        for (ICompilerProblem problem : requestableProblems.getProblemQuery().getProblems())
+        {
             Class aClass = problem.getClass();
             Annotation[] annotations = aClass.getAnnotations();
 
-            for (Annotation annotation : annotations) {
-                if (annotation instanceof DefaultSeverity) {
+            for (Annotation annotation : annotations)
+            {
+                if (annotation instanceof DefaultSeverity)
+                {
                     DefaultSeverity myAnnotation = (DefaultSeverity) annotation;
                     CompilerProblemSeverity cps = myAnnotation.value();
                     String level;
@@ -244,13 +295,13 @@ public class Tool {
                         level = Message.WARNING;
                     else
                         break; // skip if IGNORE?
-                    CompilerMessage msg = new CompilerMessage(level,
-                            problem.getSourcePath(),
-                            problem.getLine() + 1,
+                    CompilerMessage msg = new CompilerMessage(level, problem.getSourcePath(), problem.getLine() + 1,
                             problem.getColumn());
-                    try {
+                    try
+                    {
                         String errText = (String) aClass.getField("DESCRIPTION").get(aClass);
-                        while (errText.contains("${")) {
+                        while (errText.contains("${"))
+                        {
                             int start = errText.indexOf("${");
                             int end = errText.indexOf("}", start);
                             String token = errText.substring(start + 2, end);
@@ -261,16 +312,24 @@ public class Tool {
 
                         msg.setMessage(errText);
                         logMessage(msg);
-                    } catch (IllegalArgumentException e) {
+                    }
+                    catch (IllegalArgumentException e)
+                    {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
-                    } catch (SecurityException e) {
+                    }
+                    catch (SecurityException e)
+                    {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
-                    } catch (IllegalAccessException e) {
+                    }
+                    catch (IllegalAccessException e)
+                    {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
-                    } catch (NoSuchFieldException e) {
+                    }
+                    catch (NoSuchFieldException e)
+                    {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
@@ -280,44 +339,61 @@ public class Tool {
         }
     }
 
-    private static void logMessage(CompilerMessage msg) {
+    private static void logMessage(CompilerMessage msg)
+    {
 
-        final Logger logger = ThreadLocalToolkit.getLogger();
+        Logger logger = ThreadLocalToolkit.getLogger() != null ? ThreadLocalToolkit.getLogger() : new ConsoleLogger();
 
-        if (logger != null) {
-            if (msg.getLevel().equals(Message.INFO)) {
+        try
+        {
+            if (msg.getLevel().equals(Message.INFO))
+            {
                 logger.logInfo(msg.getPath(), msg.getLine(), msg.getColumn(), msg.getMessage());
-            } else if (msg.getLevel().equals(Message.WARNING)) {
+            }
+            else if (msg.getLevel().equals(Message.WARNING))
+            {
                 logger.logWarning(msg.getPath(), msg.getLine(), msg.getColumn(), msg.getMessage());
-            } else if (msg.getLevel().equals(Message.ERROR)) {
+            }
+            else if (msg.getLevel().equals(Message.ERROR))
+            {
                 logger.logError(msg.getPath(), msg.getLine(), msg.getColumn(), msg.getMessage());
             }
         }
+        catch (Exception e)
+        {
+            System.out.println(msg.getMessage());
+        }
     }
 
-    public static class NoUpdateMessage extends CompilerMessage.CompilerInfo {
+    public static class NoUpdateMessage extends CompilerMessage.CompilerInfo
+    {
         private static final long serialVersionUID = 6943388392279226490L;
         public String name;
 
-        public NoUpdateMessage(String name) {
+        public NoUpdateMessage(String name)
+        {
             this.name = name;
         }
     }
 
-    static class CompilerRequestableProblems implements ProblemQueryProvider {
+    static class CompilerRequestableProblems implements ProblemQueryProvider
+    {
         private MXMLC mxmlc;
 
-        public CompilerRequestableProblems(MXMLC mxmlc) {
+        public CompilerRequestableProblems(MXMLC mxmlc)
+        {
             this.mxmlc = mxmlc;
         }
 
         @Override
-        public ProblemQuery getProblemQuery() {
+        public ProblemQuery getProblemQuery()
+        {
             return mxmlc.getProblems();
         }
     }
 
-    protected static class ArgumentBag {
+    protected static class ArgumentBag
+    {
         public String[] args;
 
         public String outputAlias;
@@ -325,7 +401,8 @@ public class Tool {
         public String newOutputPath;
         public boolean isCommandLineOutput;
 
-        public ArgumentBag(String[] args) {
+        public ArgumentBag(String[] args)
+        {
             this.args = args;
         }
     }
