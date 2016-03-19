000675c5c45ea6e9f2657109fa3c6a582716945e
Christofer Dutz <christofer.dutz@codecentric.de>
2016-03-19 14:49:16 +0100

    Made sure all files have apache headers

--- flexjs-maven-plugin/src/main/java/org/apache/flex/maven/flexjs/CompileMojo.java
+++ flexjs-maven-plugin/src/main/java/org/apache/flex/maven/flexjs/CompileMojo.java
@@ -1,5 +1,3 @@
-package org.apache.flex.maven.flexjs;
-
 /*
  * Copyright 2001-2005 The Apache Software Foundation.
  *
@@ -15,6 +13,8 @@ package org.apache.flex.maven.flexjs;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.flex.maven.flexjs;
 
 import org.apache.flex.tools.FlexTool;
 import org.apache.flex.tools.FlexToolGroup;
