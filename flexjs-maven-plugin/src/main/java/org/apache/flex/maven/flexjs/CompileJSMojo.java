704d6324b53acc2a003779dbb8c38abc16606752
Christofer Dutz <christofer.dutz@codecentric.de>
2016-04-26 16:54:14 +0200

    - Refactored the compiler mojos (as and js) to utilize the FlexToolGroups (ASJS being the default) - Enforced the FlexToolGroup "Falcon" for all externs - Added the TweenJS lib to the build

--- flexjs-maven-plugin/src/main/java/org/apache/flex/maven/flexjs/CompileJSMojo.java
+++ flexjs-maven-plugin/src/main/java/org/apache/flex/maven/flexjs/CompileJSMojo.java
@@ -16,18 +16,10 @@
 
 package org.apache.flex.maven.flexjs;
 
-import org.apache.flex.tools.FlexTool;
-import org.apache.flex.tools.FlexToolGroup;
-import org.apache.flex.tools.FlexToolRegistry;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.ProjectDependenciesResolver;
-import org.eclipse.aether.RepositorySystemSession;
 
 import java.io.File;
 
@@ -36,29 +28,33 @@ import java.io.File;
  */
 @Mojo(name="compile-js",defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class CompileJSMojo
-    extends AbstractMojo
+    extends BaseCompileMojo
 {
-
-    @Parameter(defaultValue = "${project}", readonly = true)
-    private MavenProject project;
 
     @Parameter(defaultValue="${basedir}/src/main/config/compile-js-config.xml")
     private File compileJsConfigFile;
 
-    @Parameter(defaultValue="${project.build.directory}")
-    private File outputDirectory;
-
     @Parameter(defaultValue = "${project.artifactId}-${project.version}.swc")
     private String outputFileName;
 
-    @Parameter(readonly = true, defaultValue = "${repositorySystemSession}")
-    private RepositorySystemSession repositorySystemSession;
+    @Override
+    protected File getConfigFile() {
+        return compileJsConfigFile;
+    }
 
-    @Component
-    private ProjectDependenciesResolver projectDependenciesResolver;
+    private File getOutputDirectory() {
+        return new File(outputDirectory, "generated-sources/flexjs");
+    }
 
-    public void execute()
-        throws MojoExecutionException
+    @Override
+    protected String[] getCompilerArgs(File configFile) {
+        return new String[] {"-js-output-type=FLEXJS", "-keep-asdoc", "-load-config=" + configFile.getPath(),
+                "-output=" + new File(outputDirectory.getPath(), "generated-sources/flexjs").getPath(),
+                "-define=COMPILE::AS3,false", "-define=COMPILE::JS,true"};
+    }
+
+    @Override
+    public void execute() throws MojoExecutionException
     {
         if(!compileJsConfigFile.exists()) {
             getLog().info(" - compilation config file '" + compileJsConfigFile.getPath() +
@@ -66,40 +62,16 @@ public class CompileJSMojo
             return;
         }
 
-        File generatedSourcesOutputDir = new File(outputDirectory, "generated-sources/flexjs");
+        File generatedSourcesOutputDir = getOutputDirectory();
         if(!generatedSourcesOutputDir.exists()) {
             if(!generatedSourcesOutputDir.mkdirs()) {
                 throw new MojoExecutionException("Could not create directory " + generatedSourcesOutputDir.getPath());
             }
         }
 
-        // Get the falcon tool group.
-        FlexToolRegistry toolRegistry = new FlexToolRegistry();
-        FlexToolGroup toolGroup = toolRegistry.getToolGroup("FlexJS");
-        if(toolGroup == null) {
-            throw new MojoExecutionException("Could not find tool group: Falcon");
-        }
+        super.execute();
 
-        // Get an instance of the compiler and run the build.
-        FlexTool compc = toolGroup.getFlexTool(FlexTool.FLEX_TOOL_COMPC);
-        File outputFile = new File(outputDirectory, outputFileName);
-
-/*            <!--arg value="+flexlib=${FLEX_HOME}/frameworks" />
-            <arg value="+playerglobal.version=${playerglobal.version}" />
-            <arg value="+env.PLAYERGLOBAL_HOME=${env.PLAYERGLOBAL_HOME}" />
-            <arg value="+env.AIR_HOME=${env.AIR_HOME}" />
-            <arg value="-external-library-path+=${JS.SWC}" />
-            <!-- this is not on external-library path otherwise goog.requires are not generated -->
-            <arg value="-library-path+=${GCL.SWC}" />*/
-
-        String[] args = {"-js-output-type=FLEXJS", "-keep-asdoc", "-load-config=" + compileJsConfigFile.getPath(),
-                "-output=" + new File(outputDirectory.getPath(), "generated-sources/flexjs").getPath(),
-                "-define=COMPILE::AS3,false", "-define=COMPILE::JS,true"};
-        compc.execute(args);
-
-        // Attach the file created by the compiler as artifact file to maven.
-        project.getArtifact().setFile(outputFile);
-
+        // TODO: Add Source directory
     }
 
 }
