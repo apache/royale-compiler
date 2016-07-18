aea14be67db2b28b766d274d900ce076fdb8522b
Christofer Dutz <christofer.dutz@codecentric.de>
2016-07-18 14:55:48 +0200

    - Implemented a first version of an experimental maven-extension

---
+++ flexjs-maven-extension/src/main/java/org/apache/flex/maven/flexjs/extension/FlexJsScopeSelector.java
@@ -0,0 +1,16 @@
+package org.apache.flex.maven.flexjs.extension;
+
+import org.eclipse.aether.RepositoryException;
+import org.eclipse.aether.util.graph.transformer.ConflictResolver;
+
+/**
+ * Created by christoferdutz on 18.07.16.
+ */
+public class FlexJsScopeSelector extends ConflictResolver.ScopeSelector {
+
+    @Override
+    public void selectScope(ConflictResolver.ConflictContext conflictContext) throws RepositoryException {
+
+    }
+
+}
