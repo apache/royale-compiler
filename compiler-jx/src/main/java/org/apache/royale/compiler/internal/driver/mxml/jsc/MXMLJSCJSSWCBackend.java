02576e00c360db1c24aa86ba4ab7ae0c3eb271e0
Josh Tynjala <joshtynjala@apache.org>
2018-09-10 10:19:49 -0700

    MXMLJSCJSSWCBackend: createConfigurator() should return a RoyaleProjectConfigurator instead of a Configurator, similar to MXMLRoyaleSWCBackend and MXMLJSCJSBackend

--- compiler-jx/src/main/java/org/apache/royale/compiler/internal/driver/mxml/jsc/MXMLJSCJSSWCBackend.java
+++ compiler-jx/src/main/java/org/apache/royale/compiler/internal/driver/mxml/jsc/MXMLJSCJSSWCBackend.java
@@ -38,6 +38,7 @@ import org.apache.royale.compiler.internal.codegen.mxml.jsc.MXMLJSCJSEmitter;
 import org.apache.royale.compiler.internal.driver.js.goog.JSGoogConfiguration;
 import org.apache.royale.compiler.internal.driver.mxml.MXMLBackend;
 import org.apache.royale.compiler.internal.projects.RoyaleJSProject;
+import org.apache.royale.compiler.internal.projects.RoyaleProjectConfigurator;
 import org.apache.royale.compiler.internal.targets.RoyaleSWCTarget;
 import org.apache.royale.compiler.internal.targets.JSTarget;
 import org.apache.royale.compiler.internal.visitor.as.ASNodeSwitch;
@@ -63,7 +64,7 @@ public class MXMLJSCJSSWCBackend extends MXMLBackend
     @Override
     public Configurator createConfigurator()
     {
-        return new Configurator(JSGoogConfiguration.class);
+        return new RoyaleProjectConfigurator(JSGoogConfiguration.class);
     }
 
     @Override
