9d715b936b31fc61a1d6a694beae5be35c06a556
Josh Tynjala <joshtynjala@apache.org>
2022-11-28 13:43:15 -0800

    Tests: verbatim string (references #222)

---
+++ compiler/src/test/java/as/ASVerbatimStringTests.java
@@ -0,0 +1,44 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package as;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ASVerbatimStringTests extends ASFeatureTestsBase
+{
+    @Test
+    public void testRegularStringAndVerbatimString()
+    {
+        String[] testCode = new String[]
+        {
+            "var s1:String = \"regular\\tstring\";",
+            "var s2:String = @\"verbatim\\tstring\";",
+			"assertEqual('regular string', s1, 'regular\\tstring');",
+			"assertEqual('verbatim string', s2, 'verbatim\\\\tstring');",
+        };
+        String source = getAS(new String[0], new String[0], testCode, new String[0]);
+		System.err.println("*** " + source);
+
+        compileAndRun(source);
+    }
+}
